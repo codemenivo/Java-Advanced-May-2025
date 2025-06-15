fb83a88c49bf8c648390bcb79f0bf2c0f59eadab
codeman <codemen@abv.bg>
2025-06-15 22:09:34 +0300

    Exercise Exam Examples

--- src/Exam_Examples/Matrix/P_Matrix_01_ThroneConquering_15Dec2021.java
+++ src/Exam_Examples/Matrix/P_Matrix_01_ThroneConquering_15Dec2021.java
@@ -1,6 +1,5 @@
 package Exam_Examples.Matrix;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class P_Matrix_01_ThroneConquering_15Dec2021 {
@@ -14,11 +13,105 @@ public class P_Matrix_01_ThroneConquering_15Dec2021 {
         char[][] matrix = new char[sizeField][];
         fillMatrix(sizeField, scanner, matrix);
 
+        int currentParisRow = 0;
+        int currentParisCol = 0;
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == 'P') {
+                    currentParisRow = row;
+                    currentParisCol = col;
+                    break;
+                }
+
+            }
+
+        }
+
+        while (true) {
+            String[] line = scanner.nextLine().split("\\s+");
+            String direction = line[0];
+            int enemyRow = Integer.parseInt(line[1]);
+            int enemyCol = Integer.parseInt(line[2]);
+
+            matrix[enemyRow][enemyCol] = 'S';
+            matrix[currentParisRow][currentParisCol] = '-';
+
+            switch (direction) {
+                case "up":
+                    if (currentParisRow - 1 >= 0 && energyOfParis > 0) {
+                        currentParisRow = currentParisRow - 1;
+                    }
+                    break;
+                case "down":
+                    if (currentParisRow + 1 < matrix.length && energyOfParis > 0) {
+                        currentParisRow++;
+                    }
+                    break;
+                case "left":
+                    if (currentParisCol - 1 >= 0 && energyOfParis > 0) {
+                        currentParisCol--;
+                    }
+                    break;
+                case "right":
+                    if (currentParisCol + 1 < matrix[currentParisRow].length && energyOfParis > 0) {
+                        currentParisCol++;
+                    }
+                    break;
+            }
+            energyOfParis--;
+
+            if (energyOfParis <= 0 && matrix[currentParisRow][currentParisCol] != 'H') {
+                parisDead(matrix, currentParisRow, currentParisCol);
+                return;
+            }
+
+            if (matrix[currentParisRow][currentParisCol] == 'S') {
+                energyOfParis -= 2;
+                if (energyOfParis <= 0) {
+                    parisDead(matrix, currentParisRow, currentParisCol);
+                    return;
+                } else {
+                    matrix[currentParisRow][currentParisCol] = '-';
+                }
+            } else if (matrix[currentParisRow][currentParisCol] == 'H') {
+                matrix[currentParisRow][currentParisCol] = '-';
+                System.out.printf("Paris has successfully abducted Helen! Energy left: %d%n", energyOfParis);
+                printMatrix(matrix);
+                return;
+            }
+
+
+
+        }
+
+
+
+
     }
+
+    private static void parisDead(char[][] matrix, int currentParisRow, int currentParisCol) {
+        matrix[currentParisRow][currentParisCol] = 'X';
+        System.out.printf("Paris died at %d;%d.%n", currentParisRow, currentParisCol);
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.printf("%c", matrix[row][col]);
+            }
+            System.out.println();
+        }
+    }
+
 
     private static void fillMatrix(int sizeField, Scanner scanner, char[][] matrix) {
         for (int row = 0; row < sizeField; row++) {
             matrix[row] = scanner.nextLine().toCharArray();
+
         }
     }
+
+
 }
