7c5b990a939247bffda4f3d4ef839b52804702e3
codeman <codemen@abv.bg>
2025-06-18 11:38:51 +0300

    Exercise Functional Programming

---
+++ src/L05_Functional_Programming/P05_07_FindTheSmallestElement.java
@@ -0,0 +1,34 @@
+package L05_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class P05_07_FindTheSmallestElement {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        int index = -1;
+        Function<List<Integer>, Integer> getMinElement = Collections::min;
+        int minNum = getMinElement.apply(numbers);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == minNum) {
+                index = i;
+
+            }
+        }
+        Consumer<Integer> printIndex = numIndex -> {
+            System.out.println(numIndex);
+        };
+        printIndex.accept(index);
+
+    }
+}
