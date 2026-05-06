f3e7656b6cdd48677ea93da4d48a77546e2e1eaf
codemenivo <codemen@abv.bg>
2026-05-06 07:55:44 +0300

    Java Advanced Stacks and Queues - Lab

---
+++ src/Lec_03_Stack_and_Queues/Demo_03_01_Browser_History7.java
@@ -0,0 +1,37 @@
+package Lec_03_Stack_and_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Demo_03_01_Browser_History7 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        ArrayDeque<String > stack = new ArrayDeque<String>();
+
+        while (!"Home".equals(input)){
+            if ("back".equals(input)){
+                if (stack.size() <= 1) {
+                    System.out.println("no previous URLs");
+                }else {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                }
+            }else {
+                stack.push(input);
+                System.out.println(stack.peek());
+            }
+
+            input = scanner.nextLine();
+
+        }
+
+
+
+    }
+
+
+
+}
