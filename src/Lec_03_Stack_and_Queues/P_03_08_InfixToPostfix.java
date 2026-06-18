e46d24902c0b0a0ba33ebdc455851085fc7e41d6
codemenivo <codemen@abv.bg>
2026-06-18 22:58:33 +0300

    Java Advanced Stacks and Queues - Exercises

--- src/Lec_03_Stack_and_Queues/P_03_08_InfixToPostfix.java
+++ src/Lec_03_Stack_and_Queues/P_03_08_InfixToPostfix.java
@@ -3,16 +3,17 @@ package Lec_03_Stack_and_Queues;
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
-public class P_05_08_InfixToPostfix {
+public class P_03_08_InfixToPostfix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
 
         String[] input = scanner.nextLine().split("\\s+");
         StringBuilder sb = new StringBuilder();
         ArrayDeque<String> stack = new ArrayDeque<>();
 
         for (int i = 0; i < input.length; i++) {
-            String currentStr =  input[i];
+            String currentStr = input[i];
             if (currentStr.equals("(")) {
                 stack.push(currentStr);
             } else if (currentStr.equals(")")) {
@@ -58,4 +59,6 @@ public class P_05_08_InfixToPostfix {
         return currentStr.equals("+") || currentStr.equals("-")
                 || currentStr.equals("*") || currentStr.equals("/");
     }
+
+
 }
